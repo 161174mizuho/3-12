1961e30fb4b7cd841d3438937454ce7236243915
EC2 Default User <ec2-user@ip-172-31-37-102.ec2.internal>
2022-01-26 15:28:19 +0000

    [update]3-12destolyエラーメッセージIF分文を使って追加/CSS編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,14 @@
 class BooksController < ApplicationController
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -13,6 +18,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    
   end
   
   def edit
@@ -20,9 +26,16 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])#editから送られてきた編集したデータをbookに入れる
-    book.update(book_params)#bookに入れた内容で更新する
-    redirect_to book_path(book.id)
+    book = Book.find(params[:id])
+    if
+      book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      redirect_to edit_book_path
+    end
+
+      
   end
   
   def destroy
